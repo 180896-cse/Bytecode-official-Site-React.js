c49673e5bc2118dfbae9bd89e047f47a0d442257
Shantanu_Pratap <62785261+180896-cse@users.noreply.github.com>
2021-01-06 19:06:49 +0530

    rebooted

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import React, { Component, useState } from "react";
-import "./styles.css";
-import "tachyons";
-import Form from "./component/Form";
-import firebase from "./util/firebase";
-import TodoList from "./component/TodoList";
-export default function App() {
-  const [todoList, setTodoList] = useState();
-  useEffect(() => {
-    const todoRef = firebase.database().ref("Todo");
-    todoRef.on("value", (snapshot) => {
-      const todos = snapshot.val();
-      const todoList = [];
-      for (let id in todos) {
-        todoList.push(todos[id]);
-      }
-      console.log(todoList);
-      setTodoList(todoList);
-    });
-  }, []);
+import React from "react";
+import ReactDom from "react-dom";
 
-  {
+class App extends React.Component {
+  render() {
     return (
-      <div className="App">
-        <h1>TO DO APP</h1>
-        <Form />
-        <TodoList todoList={todoList} />
+      <div>
+        <h1>hi its me app.js</h1>
       </div>
     );
   }
 }
+export default App;
